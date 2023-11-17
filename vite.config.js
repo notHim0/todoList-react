45aa1753c645d2f8a69ce6bb8ba6198c29c6e27a
Soham Gupta <soham.gupta003@gmail.com>
2023-11-17 17:58:58 +0530

    Updated gh-pages config files

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/todoList-react",
+  build: {
+    outDir: 'build',
+  },
   plugins: [react()],
 })
